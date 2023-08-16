b8215c3e61b419eab3f5ad22a4c6643668e3579b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-08-16 11:36:24 +0300

    Create 04-Constructor.sol

---
+++ 04-Constructor.sol
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+/*
+A constructor is a special function that is executed only once when a contract is deployed. 
+The purpose of the constructor is to initialize the state variables of the contract.
+*/
