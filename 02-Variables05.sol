7c2902849b2064c48d373d7c1fdea27526ef0db7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-08-16 10:09:05 +0300

    Create 02-Variables05.sol

---
+++ 02-Variables05.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+//Constant variables
+
+contract HelloWeb3 {
+    uint256 public constant trYear = 1923;
+}
+
+/*
+Constant variables are variables whose value cannot be changed once they are defined. 
+Constant variables can be of any data type. 
+Constant variables are used to store data that is 
+known at compile-time and does not need to be changed during the execution of the smart contract.
+*/
